69931ce1010fa36bd95556f4dc59740269de5f99
alex3wielki <aleksander3wielki@gmail.com>
2017-04-20 22:26:37 -0400

    reduced the animation time

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,6 +1,12 @@
 (() => {
     console.debug('App started');
-    // Makes the .toTop button appear
+    /**
+     * I wasn't sure if I should hide the button, or make it visible all the time for someone with js disabled.
+     * I decided to trust the user with disabled js and give them the full experience without js.
+     * JS will just make it look preety.
+     */
+    $('.toTop').hide();
+    // Makes the .toTop button appear depending on user's position
     $(window).on('scroll', function() {
             if ($(window).scrollTop() > ($('#books').offset().top) * 1.5) {
                 $('.toTop').fadeIn(500);
@@ -8,10 +14,15 @@
                 $('.toTop').fadeOut(500);
             }
         })
-        // Making .toTop animate
+        /**
+         * Making .toTop animate.
+         * I decided to go with a longer scroll time.
+         * I didn't want user to feel lost.
+         * + if the user has a slow machine, a quick scroll would look broken.
+         */
     $('.toTop').on('click', function(event) {
         event.preventDefault();
-        $('html,body').animate({ scrollTop: 0 }, 1000);
+        $('html,body').animate({ scrollTop: 0 }, 800);
     })
 
 })()
